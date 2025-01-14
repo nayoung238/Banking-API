bd4b91be8424544af973bfbddfa8a33447e15c64
JinjinBread <pge0720@naver.com>
2025-01-14 22:56:42 +0900

    test: 계좌 번호 생성 로직 테스트 코드 추가

--- src/test/java/SN/BANK/account/service/AccountServiceTest.java
+++ src/test/java/SN/BANK/account/service/AccountServiceTest.java
@@ -30,24 +30,20 @@ class AccountServiceTest {
     @InjectMocks
     AccountService accountService;
 
-    private CreateAccountRequest createAccountRequest;
-    private Users user;
-
-    @BeforeEach
-    void setUp() {
-        createAccountRequest = new CreateAccountRequest();
-        createAccountRequest.setPassword("password123");
-        createAccountRequest.setCurrency(Currency.대한민국);
-
-        user = new Users("테스트이름", "test1234", "1234");
-    }
-
     @Test
     @DisplayName("계좌 개설 성공 테스트")
     void createAccount() {
 
         // given
-        when(usersRepository.findById(eq(1L))).thenReturn(Optional.ofNullable(user));
+        CreateAccountRequest createAccountRequest =
+                CreateAccountRequest.builder()
+                        .password("1234")
+                        .currency(Currency.KRW)
+                        .build();
+
+        Users user = new Users("테스트이름", "test1234", "test1234");
+
+        when(usersRepository.findById(eq(1L))).thenReturn(Optional.of(user));
         when(accountRepository.existsByAccountNumber(any(String.class))).thenReturn(false);
         // save 메서드 호출 시 전달된 객체를 그대로 반환하도록.
         when(accountRepository.save(any(Account.class))).thenAnswer(invocationOnMock -> invocationOnMock.getArgument(0));
@@ -62,6 +58,16 @@ class AccountServiceTest {
                 () -> Assertions.assertEquals(createdAccount.getUser().getLoginId(), "test1234"),
                 () -> Assertions.assertEquals(createdAccount.getAccountNumber().length(), 14)
         );
+    }
+
+    @Test
+    @DisplayName("14자의 숫자로 이루어진 계좌번호를 생성할 수 있다.")
+    void generateAccountNumber_success() {
+        String accountNumber = accountService.generateAccountNumber();
+
+        Assertions.assertNotNull(accountNumber);
+        Assertions.assertEquals(accountNumber.length(), 14);
+        Assertions.assertTrue(accountNumber.matches("\\d{14}"));
     }
 
 }
