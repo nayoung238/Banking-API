6560a98005f67eff763c2b6b3a53c9bc55b349b8
JinjinBread <pge0720@naver.com>
2025-01-14 13:16:37 +0900

    initialize

---
+++ src/test/java/SN/BANK/SnBankApplicationTests.java
@@ -0,0 +1,13 @@
+package SN.BANK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
