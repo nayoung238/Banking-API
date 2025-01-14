639b22eb9a446f257f4646ee1284b8ccc0ecca28
JinjinBread <pge0720@naver.com>
2025-01-14 20:47:03 +0900

    test: 계좌 개설 기능 테스트 코드 작성

---
+++ src/test/java/SN/BANK/account/service/AccountServiceTest.java
@@ -0,0 +1,67 @@
+package SN.BANK.account.service;
+
+import SN.BANK.account.dto.request.CreateAccountRequest;
+import SN.BANK.account.repository.AccountRepository;
+import SN.BANK.account.entity.Account;
+import SN.BANK.domain.Users;
+import SN.BANK.domain.enums.Currency;
+import SN.BANK.user.repository.UsersRepository;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(SpringExtension.class)
+class AccountServiceTest {
+
+    @Mock
+    AccountRepository accountRepository;
+
+    @Mock
+    UsersRepository usersRepository;
+
+    @InjectMocks
+    AccountService accountService;
+
+    private CreateAccountRequest createAccountRequest;
+    private Users user;
+
+    @BeforeEach
+    void setUp() {
+        createAccountRequest = new CreateAccountRequest();
+        createAccountRequest.setPassword("password123");
+        createAccountRequest.setCurrency(Currency.대한민국);
+
+        user = new Users("테스트이름", "test1234", "1234");
+    }
+
+    @Test
+    @DisplayName("계좌 개설 성공 테스트")
+    void createAccount() {
+
+        // given
+        when(usersRepository.findById(eq(1L))).thenReturn(Optional.ofNullable(user));
+        when(accountRepository.existsByAccountNumber(any(String.class))).thenReturn(false);
+        // save 메서드 호출 시 전달된 객체를 그대로 반환하도록.
+        when(accountRepository.save(any(Account.class))).thenAnswer(invocationOnMock -> invocationOnMock.getArgument(0));
+
+        // when
+        Account createdAccount = accountService.createAccount(1L, createAccountRequest);
+
+        // then
+        Assertions.assertNotNull(createdAccount);
+        Assertions.assertAll(
+                () -> Assertions.assertEquals(createdAccount.getAccountName(), "SN은행-계좌"),
+                () -> Assertions.assertEquals(createdAccount.getUser().getLoginId(), "test1234"),
+                () -> Assertions.assertEquals(createdAccount.getAccountNumber().length(), 14)
+        );
+    }
+
+}
