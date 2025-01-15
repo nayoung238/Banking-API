cb8b1a786f3ed7bdc67c4cd37a848038e8df81c8
junhwan98 <gensh666@naver.com>
2025-01-15 16:03:46 +0900

    refactor: 불필요한 코드 제거

--- src/main/java/SN/BANK/payment/dto/PaymentRefundRequestDto.java
+++ src/main/java/SN/BANK/payment/dto/PaymentRefundRequestDto.java
@@ -1,12 +1,7 @@
 package SN.BANK.payment.dto;
 
-
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
 import lombok.Getter;
 
 
