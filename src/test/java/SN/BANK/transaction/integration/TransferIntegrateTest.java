263f0fc56d0041f7482476a707198aac40d87f0a
JinjinBread <pge0720@naver.com>
2025-01-17 00:00:06 +0900

    test: 이체 내역 전체 조회 및 단건 조회 기능 통합 테스트 코드 작성

--- src/test/java/SN/BANK/transaction/integration/TransferIntegrateTest.java
+++ src/test/java/SN/BANK/transaction/integration/TransferIntegrateTest.java
@@ -3,11 +3,15 @@ package SN.BANK.transaction.integration;
 import SN.BANK.account.entity.Account;
 import SN.BANK.account.repository.AccountRepository;
 import SN.BANK.account.enums.Currency;
+import SN.BANK.transaction.dto.request.TransactionFindDetailRequest;
 import SN.BANK.transaction.dto.request.TransactionRequest;
+import SN.BANK.transaction.dto.response.TransactionResponse;
 import SN.BANK.transaction.enums.TransactionType;
+import SN.BANK.transaction.service.TransactionService;
 import SN.BANK.users.entity.Users;
 import SN.BANK.users.repository.UsersRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -23,6 +27,9 @@ import org.springframework.transaction.annotation.Transactional;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+import static org.hamcrest.Matchers.everyItem;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -42,6 +49,9 @@ class TransferIntegrateTest {
 
     @Autowired
     AccountRepository accountRepository;
+
+    @Autowired
+    TransactionService transactionService;
 
     ObjectMapper objectMapper = new ObjectMapper();
     MockHttpSession session;
@@ -78,7 +88,7 @@ class TransferIntegrateTest {
 
         account2 = Account.builder()
                 .user(user2)
-                .password("1234")
+                .password("4321")
                 .accountNumber("22222222222222")
                 .money(BigDecimal.valueOf(10000))
                 .accountName("test account2")
@@ -92,12 +102,13 @@ class TransferIntegrateTest {
         account2 = accountRepository.save(account2);
 
         session = new MockHttpSession();
-        session.setAttribute("user", user1.getId());
     }
 
     @Test
     @DisplayName("이체 성공 테스트")
     void transfer() throws Exception {
+
+        session.setAttribute("user", user1.getId());
 
         BigDecimal amount = BigDecimal.valueOf(5000);
         BigDecimal balance = account1.getMoney().subtract(amount);
@@ -122,6 +133,78 @@ class TransferIntegrateTest {
                 .andExpect(jsonPath("$.balance").value(balance))
                 .andDo(print());
 
+    }
+
+    @Test
+    @DisplayName("모든 이체 내역 조회 테스트")
+    void findAllTransaction() throws Exception {
+
+        session.setAttribute("user", user1.getId());
+
+        BigDecimal amount = BigDecimal.valueOf(5000);
+
+        TransactionRequest transactionRequest1 =
+                TransactionRequest.builder()
+                        .accountPassword("1234")
+                        .senderAccountId(account1.getId())
+                        .receiverAccountId(account2.getId())
+                        .amount(amount)
+                        .build();
+
+        TransactionRequest transactionRequest2 =
+                TransactionRequest.builder()
+                        .accountPassword("4321")
+                        .senderAccountId(account2.getId())
+                        .receiverAccountId(account1.getId())
+                        .amount(amount)
+                        .build();
+
+        transactionService.createTransaction(user1.getId(), transactionRequest1);
+        transactionService.createTransaction(user2.getId(), transactionRequest2);
+
+        mockMvc.perform(get("/transfer/history/{accountId}", account1.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .session(session))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(2)) // 거래내역 2개
+                .andExpect(jsonPath("$[*].accountNumber", everyItem(is(account1.getAccountNumber()))))
+                .andExpect(jsonPath("$[*].balance", Matchers.containsInAnyOrder(5000, 10000)))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("이체 내역 단건 조회 테스트")
+    void findTransaction() throws Exception {
+
+        session.setAttribute("user", user1.getId());
+
+        BigDecimal amount = BigDecimal.valueOf(5000);
+        BigDecimal balance = account1.getMoney().subtract(amount);
+
+        TransactionRequest transactionRequest =
+                TransactionRequest.builder()
+                        .accountPassword("1234")
+                        .senderAccountId(account1.getId())
+                        .receiverAccountId(account2.getId())
+                        .amount(amount)
+                        .build();
+
+        TransactionResponse tx = transactionService.createTransaction(user1.getId(), transactionRequest);
+
+        TransactionFindDetailRequest txFindDetailRequest = TransactionFindDetailRequest.builder()
+                .accountId(user1.getId())
+                .transactionId(tx.getTransactionId())
+                .build();
+
+        mockMvc.perform(get("/transfer/history/detail")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .session(session)
+                        .content(objectMapper.writeValueAsString(txFindDetailRequest)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.transactionType").value(TransactionType.WITHDRAWAL.name()))
+                .andExpect(jsonPath("$.amount").value(amount))
+                .andExpect(jsonPath("$.balance").value(balance))
+                .andDo(print());
     }
 
 
