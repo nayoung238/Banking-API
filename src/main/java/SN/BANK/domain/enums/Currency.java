750b3b4ca233d466d7fcd9ad52edce00889ad405
Jieun <92997924+JinjinBread@users.noreply.github.com>
2025-01-17 13:49:01 +0900

    Revert "feat: 계좌 개설 및 조회 기능 추가"

--- src/main/java/SN/BANK/domain/enums/Currency.java
+++ src/main/java/SN/BANK/domain/enums/Currency.java
@@ -5,6 +5,10 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum Currency {
 
-    KRW, USD, EUR;
+    대한민국("KRW"),
+    미국("USD"),
+    유럽("EUR");
+
+    private final String currency;
 
 }
