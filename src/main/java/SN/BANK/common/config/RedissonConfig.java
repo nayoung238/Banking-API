165d6a43d938e8b9869d65e75b1651b924df15e1
hen715 <hen715@naver.com>
2025-01-23 13:54:28 +0900

    feat: 레디스 ip 변경

--- src/main/java/SN/BANK/common/config/RedissonConfig.java
+++ src/main/java/SN/BANK/common/config/RedissonConfig.java
@@ -3,16 +3,19 @@ package SN.BANK.common.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RedissonConfig {
+    @Value("${serverIp}")
+    private String server;
 
     @Bean
     public RedissonClient redissonClient() {
         Config config = new Config();
-        config.useSingleServer().setAddress("redis://127.0.0.1:6379");
+        config.useSingleServer().setAddress("redis://"+server+":6379");
         return Redisson.create(config);
     }
 }
