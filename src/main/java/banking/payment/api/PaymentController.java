0db188528ec17f48c5da950e630c113645a2587d
imzero238 <nayoung8142@gmail.com>
2025-03-07 20:47:23 +0900

    feat: @AuthenticationPrincipal 설정

--- src/main/java/banking/payment/api/PaymentController.java
+++ src/main/java/banking/payment/api/PaymentController.java
@@ -1,5 +1,6 @@
 package banking.payment.api;
 
+import banking.auth.entity.UserPrincipal;
 import banking.common.aop.Decrypt;
 import banking.payment.dto.request.PaymentRefundRequestDto;
 import banking.payment.dto.request.PaymentRequestDto;
@@ -12,10 +13,10 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -32,18 +33,21 @@ public class PaymentController {
      * @param paymentRequest 결제 요청 데이터를 담은 DTO
      * @return paymentId를 담은 응답 DTO
      */
-    @Operation(summary = "결제", description = "바디에 {withdrawAccountNumber, depositAccountNumber,amount, password}을 json 형식으로 보내주세요.")
+    @Operation(
+        summary = "결제",
+        description = "바디에 {withdrawAccountNumber, depositAccountNumber,amount, password} json 형식 추가 & Request Header에 Access Token 설정"
+    )
     @ApiResponses({
         @ApiResponse(responseCode = "200", description = "결제 성공", content = @Content(schema = @Schema(implementation = PaymentResponseDto.class))),
-        @ApiResponse(responseCode = "404", description = "존재하지 않는 계좌입니다.", content = @Content(schema = @Schema(implementation = String.class))),
-        @ApiResponse(responseCode = "401", description = "비밀번호가 일치하지 않습니다.", content = @Content(schema = @Schema(implementation = String.class))),
-        @ApiResponse(responseCode = "400", description = "잔액이 부족합니다. / 같은 계좌 간 이체는 불가합니다.", content = @Content(schema = @Schema(implementation = String.class)))
+        @ApiResponse(responseCode = "404", description = "존재하지 않는 계좌.", content = @Content(schema = @Schema(implementation = String.class))),
+        @ApiResponse(responseCode = "401", description = "비밀번호 불알치", content = @Content(schema = @Schema(implementation = String.class))),
+        @ApiResponse(responseCode = "400", description = "잔액 부족 / 같은 계좌 간 이체 불가", content = @Content(schema = @Schema(implementation = String.class)))
     })
     @PostMapping
-    @Decrypt
-    public ResponseEntity<?> processPayment(@Valid @RequestBody PaymentRequestDto paymentRequest, HttpServletRequest httpServletRequest) {
-        Long requesterId = Long.valueOf(httpServletRequest.getHeader("X-User-Id"));
-        PaymentResponseDto response = paymentService.processPayment(requesterId, paymentRequest);
+//    @Decrypt
+    public ResponseEntity<?> processPayment(@Valid @RequestBody PaymentRequestDto paymentRequest,
+                                            @AuthenticationPrincipal UserPrincipal userPrincipal) {
+        PaymentResponseDto response = paymentService.processPayment(userPrincipal.getId(), paymentRequest);
         return ResponseEntity.ok(response);
     }
 
@@ -53,17 +57,17 @@ public class PaymentController {
      * @param refundRequest 환불하려는 결제 내역 ID
      * @return 결제 취소 결과 DTO
      */
-    @Operation(summary = "결제 취소", description = "바디에 {paymentId, password}을 json 형식으로 보내주세요.")
+    @Operation(summary = "결제 취소", description = "바디에 {paymentId, password}을 json 형식 추가 & Request Header에 Access Token 설정")
     @ApiResponses({
         @ApiResponse(responseCode = "200", description = "결제취소 완료", content = @Content(schema = @Schema(implementation = String.class))),
-        @ApiResponse(responseCode = "401", description = "비밀번호가 일치하지 않습니다.", content = @Content(schema = @Schema(implementation = String.class))),
-        @ApiResponse(responseCode = "400", description = "이미 결제 취소된 내역입니다.", content = @Content(schema = @Schema(implementation = String.class)))
+        @ApiResponse(responseCode = "401", description = "비밀번호 불일치", content = @Content(schema = @Schema(implementation = String.class))),
+        @ApiResponse(responseCode = "400", description = "이미 취소된 결제", content = @Content(schema = @Schema(implementation = String.class)))
     })
     @PostMapping("/cancel")
-    @Decrypt
-    public ResponseEntity<?> refundPayment(@Valid @RequestBody PaymentRefundRequestDto refundRequest, HttpServletRequest httpServletRequest) {
-        Long requesterId = Long.valueOf(httpServletRequest.getHeader("X-User-Id"));
-        RefundPaymentResponseDto response = paymentService.refundPayment(requesterId, refundRequest);
+//    @Decrypt
+    public ResponseEntity<?> refundPayment(@Valid @RequestBody PaymentRefundRequestDto refundRequest,
+                                           @AuthenticationPrincipal UserPrincipal userPrincipal) {
+        RefundPaymentResponseDto response = paymentService.refundPayment(userPrincipal.getId(), refundRequest);
         return ResponseEntity.ok(response);
     }
 
@@ -73,15 +77,15 @@ public class PaymentController {
      * @param paymentId 조회하려는 결제 ID
      * @return 특정 결제 내역 응답 DTO
      */
-    @Operation(summary = "결제 내역 조회", description = "url 변수에 결제 id를 보내주세요.")
+    @Operation(summary = "결제 내역 조회", description = "url 변수에 결제 id 설정 & Request Header에 Access Token 설정")
     @ApiResponses({
         @ApiResponse(responseCode = "200", description = "결제 내역 조회 완료", content = @Content(schema = @Schema(implementation = PaymentResponseDto.class))),
-        @ApiResponse(responseCode = "404", description = "결제 내역이 존재하지 않습니다.", content = @Content(schema = @Schema(implementation = String.class)))
+        @ApiResponse(responseCode = "404", description = "존재하지 않는 결제 내역", content = @Content(schema = @Schema(implementation = String.class)))
     })
     @GetMapping("/{paymentId}")
-    public ResponseEntity<?> findPaymentDetail(@PathVariable("paymentId") Long paymentId, HttpServletRequest httpServletRequest) {
-        Long requesterId = Long.valueOf(httpServletRequest.getHeader("X-User-Id"));
-        PaymentResponseDto response = paymentService.findPaymentById(requesterId, paymentId);
+    public ResponseEntity<?> findPaymentDetail(@PathVariable("paymentId") Long paymentId,
+                                               @AuthenticationPrincipal UserPrincipal userPrincipal) {
+        PaymentResponseDto response = paymentService.findPaymentById(userPrincipal.getId(), paymentId);
         return ResponseEntity.ok(response);
     }
 
--- src/main/java/banking/transfer/api/TransferController.java
+++ src/main/java/banking/transfer/api/TransferController.java
@@ -1,5 +1,6 @@
 package banking.transfer.api;
 
+import banking.auth.entity.UserPrincipal;
 import banking.transfer.dto.request.TransferDetailsRequestDto;
 import banking.transfer.dto.request.TransferRequestDto;
 import banking.transfer.dto.response.TransferSimpleResponseDto;
@@ -12,11 +13,11 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -28,48 +29,48 @@ public class TransferController {
     private final TransferService transferService;
     private final TransferQueryService transferQueryService;
 
-    @Operation(summary = "이체", description = "바디에 {senderAccountId, receiverAccountId,amount, accountPassword}을 json 형식으로 보내주세요. 세션에 연결되어 있어야합니다.")
+    @Operation(summary = "이체", description = "바디에 {senderAccountId, receiverAccountId,amount, accountPassword}을 json 형식 추가 & Request Header에 Access Token 설정")
     @ApiResponses({
         @ApiResponse(responseCode = "200", description = "송금 성공", content = @Content(schema = @Schema(implementation = TransferDetailsResponseDto.class))),
-        @ApiResponse(responseCode = "404", description = "존재하지 않는 계좌입니다.", content = @Content(schema = @Schema(implementation = String.class))),
-        @ApiResponse(responseCode = "403", description = "해당 계좌에 대한 접근 권한이 없습니다.", content = @Content(schema = @Schema(implementation = String.class))),
-        @ApiResponse(responseCode = "401", description = "비밀번호가 일치하지 않습니다.", content = @Content(schema = @Schema(implementation = String.class))),
-        @ApiResponse(responseCode = "400", description = "잔액이 부족합니다. / 같은 계좌 간 이체는 불가합니다.", content = @Content(schema = @Schema(implementation = String.class))),
-        @ApiResponse(responseCode = "500", description = "환율 값은 0보다 커야 합니다.", content = @Content(schema = @Schema(implementation = String.class)))
+        @ApiResponse(responseCode = "404", description = "존재하지 않는 계좌", content = @Content(schema = @Schema(implementation = String.class))),
+        @ApiResponse(responseCode = "403", description = "해당 계좌에 대한 접근 권한 없음", content = @Content(schema = @Schema(implementation = String.class))),
+        @ApiResponse(responseCode = "401", description = "비밀번호 불일치", content = @Content(schema = @Schema(implementation = String.class))),
+        @ApiResponse(responseCode = "400", description = "잔액 부족 / 같은 계좌 간 이체 불가", content = @Content(schema = @Schema(implementation = String.class))),
+        @ApiResponse(responseCode = "500", description = "환율 값 0보다 작은 경우", content = @Content(schema = @Schema(implementation = String.class)))
     })
     @PostMapping
-    public ResponseEntity<?> transfer(@RequestBody @Valid TransferRequestDto transferRequest, HttpServletRequest httpServletRequest) {
-        Long requesterId = Long.valueOf(httpServletRequest.getHeader("X-User-Id"));
+    public ResponseEntity<?> transfer(@RequestBody @Valid TransferRequestDto transferRequest,
+                                      @AuthenticationPrincipal UserPrincipal userPrincipal) {
         return ResponseEntity
                 .status(HttpStatus.OK)
-                .body(transferService.transfer(requesterId, transferRequest));
+                .body(transferService.transfer(userPrincipal.getId(), transferRequest));
     }
 
-    @Operation(summary = "계좌의 모든 거래 내역 조회", description = "url 변수에 계좌의 id를 보내주세요. 세션에 연결되어 있어야합니다.")
+    @Operation(summary = "계좌의 모든 거래 내역 조회", description = "url 변수에 계좌의 id 추가 & Request Header에 Access Token 설정")
     @ApiResponses({
         @ApiResponse(responseCode = "200", description = "계좌의 모든 거래 조회 완료", content = @Content(schema = @Schema(implementation = TransferSimpleResponseDto.class))),
-        @ApiResponse(responseCode = "404", description = "존재하지 않는 계좌입니다.", content = @Content(schema = @Schema(implementation = String.class))),
-        @ApiResponse(responseCode = "403", description = "해당 계좌에 대한 접근 권한이 없습니다.", content = @Content(schema = @Schema(implementation = String.class)))
+        @ApiResponse(responseCode = "404", description = "존재하지 않는 계좌", content = @Content(schema = @Schema(implementation = String.class))),
+        @ApiResponse(responseCode = "403", description = "해당 계좌에 대한 접근 권한 없음", content = @Content(schema = @Schema(implementation = String.class)))
     })
     @GetMapping("/history/{accountId}")
-    public ResponseEntity<?> findAllTransferSimple(@PathVariable("accountId") Long accountId, HttpServletRequest httpServletRequest) {
-        Long requesterId = Long.valueOf(httpServletRequest.getHeader("X-User-Id"));
+    public ResponseEntity<?> findAllTransferSimple(@PathVariable("accountId") Long accountId,
+                                                   @AuthenticationPrincipal UserPrincipal userPrincipal) {
         return ResponseEntity
                 .status(HttpStatus.OK)
-                .body(transferQueryService.findAllTransferSimple(requesterId, accountId));
+                .body(transferQueryService.findAllTransferSimple(userPrincipal.getId(), accountId));
     }
 
-    @Operation(summary = "거래 상세 내역 조회", description = "바디에 {accountId, transactionId}을 json 형식으로 보내주세요. 세션에 연결되어 있어야합니다.")
+    @Operation(summary = "거래 상세 내역 조회", description = "바디에 {accountId, transactionId}을 json 형식 추가 & Request Header에 Access Token 설정")
     @ApiResponses({
-        @ApiResponse(responseCode = "200", description = "거래 상세내역 조회 완료", content = @Content(schema = @Schema(implementation = TransferSimpleResponseDto.class))),
-        @ApiResponse(responseCode = "404", description = "존재하지 않는 계좌입니다. / 존재하지 않는 거래내역입니다.", content = @Content(schema = @Schema(implementation = String.class))),
-        @ApiResponse(responseCode = "403", description = "해당 계좌에 대한 접근 권한이 없습니다.", content = @Content(schema = @Schema(implementation = String.class)))
+        @ApiResponse(responseCode = "200", description = "거래 상세 내역 조회 완료", content = @Content(schema = @Schema(implementation = TransferSimpleResponseDto.class))),
+        @ApiResponse(responseCode = "404", description = "존재하지 않는 계좌 / 존재하지 않는 거래내역", content = @Content(schema = @Schema(implementation = String.class))),
+        @ApiResponse(responseCode = "403", description = "해당 계좌에 대한 접근 권한 없음", content = @Content(schema = @Schema(implementation = String.class)))
     })
     @GetMapping
-    public ResponseEntity<?> findTransferDetails( @RequestBody @Valid TransferDetailsRequestDto request, HttpServletRequest httpServletRequest) {
-        Long requesterId = Long.valueOf(httpServletRequest.getHeader("X-User-Id"));
+    public ResponseEntity<?> findTransferDetails(@RequestBody @Valid TransferDetailsRequestDto request,
+                                                 @AuthenticationPrincipal UserPrincipal userPrincipal) {
         return ResponseEntity
                 .status(HttpStatus.OK)
-                .body(transferQueryService.findTransferDetails(requesterId, request));
+                .body(transferQueryService.findTransferDetails(userPrincipal.getId(), request));
     }
 }
